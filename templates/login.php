99efe50c8696b0d68cf72a560e1799ab446d34a9
Raphaël Vlaminck <78857448+RaphaelVlaminck@users.noreply.github.com>
2021-06-03 17:30:03 +0200

    Add files via upload

--- templates/login.php
+++ templates/login.php
@@ -15,68 +15,61 @@ if ($checked = valider("remember", "COOKIE")) $checked = "checked";
 
 
 ?>
-<div id="login_container">
-  <div id="login_gauche">
-    <div class="page-header">
-      <h1>Se connecter</h1>
-    </div>
+<div id="Connexion">
+<div class="page-header">
+  <h1>Connexion</h1>
+</div>
 
-    <?php if ($alerte = valider("alertelogin")){
-      echo "<p style='color:red;'>$alerte</p>";
-    }
-    ?>
+<?php if ($alerte = valider("alerte")){
+  echo "<p style='color:red;'>$alerte</p>";
+}
+?>
 
-    <p class="lead">
+<p class="lead" >
 
-     <form role="form" action="controleur.php" >
-      <div class="form-group">
-        <label for="email">Login</label>
-        <input type="text" class="form-control" id="email" name="login" value="<?php echo $login;?>" >
-      </div>
-      <div class="form-group">
-        <label for="pwd">Passe</label>
-        <input type="password" class="form-control" id="pwd" name="passe" value="<?php echo $passe;?>">
-      </div>
-      <div class="checkbox">
-        <label><input type="checkbox" name="remember" <?php echo $checked;?> >Se souvenir de moi</label>
-      </div>
-      <button type="submit" name="action" value="Connexion" class="btn btn-default">Connexion</button>
-    </form>
-
-    </p>
+ <form role="form" action="controleur.php" >
+  <div class="form-group">
+    <label for="email">Login</label>
+    <input type="text" class="form-control" id="email" name="login" value="<?php echo $login;?>" >
   </div>
-
-
-
-  <div id="login_droite">
-    <div class="page-header">
-      <h1>Créer son compte</h1>
-    </div>
-
-    <?php if ($alerte = valider("alerteregister")){
-      echo "<p style='color:red;'>$alerte</p>";
-    }
-    ?>
-
-    <p class="lead">
-
-     <form role="form" action="controleur.php" method="POST">
-      <div class="form-group">
-        <label for="email">Login</label>
-        <input type="text" class="form-control" id="email" name="login" value="<?php echo $login;?>" >
-      </div>
-      <div class="form-group">
-        <label for="pwd">Passe</label>
-        <input type="password" class="form-control" id="pwd" name="passe" value="<?php echo $passe;?>">
-      </div>
-      <div class="checkbox">
-        <label><input type="checkbox" name="remember" <?php echo $checked;?> >Se souvenir de moi</label>
-      </div>
-      <button type="submit" name="action" value="Créer un Compte" class="btn btn-default">Créer un Compte</button>
-    </form>
-
-    </p>
+  <div class="form-group">
+    <label for="pwd">Passe</label>
+    <input type="password" class="form-control" id="pwd" name="passe" value="<?php echo $passe;?>">
   </div>
+  <div class="checkbox">
+    <label><input type="checkbox" name="remember" <?php echo $checked;?> >Se souvenir de moi</label>
+  </div>
+  <button type="submit" name="action" value="Connexion" class="btn btn-default">Connexion</button>
+</form>
+
+</p>
+</div>
+
+<div id="Inscription">
+<div class="page-header">
+  <h1>Création de Compte</h1>
+</div>
+
+
+
+<p class="lead" >
+
+ <form role="form" action="controleur.php" method="POST">
+  <div class="form-group">
+    <label for="email">Login</label>
+    <input type="text" class="form-control" id="email" name="login" value="<?php echo $login;?>" >
+  </div>
+  <div class="form-group">
+    <label for="pwd">Passe</label>
+    <input type="password" class="form-control" id="pwd" name="passe" value="<?php echo $passe;?>">
+  </div>
+  <div class="checkbox">
+    <label><input type="checkbox" name="remember" <?php echo $checked;?> >Se souvenir de moi</label>
+  </div>
+  <button type="submit" name="action" value="Créer un Compte" class="btn btn-default">Créer un Compte</button>
+</form>
+
+</p>
 </div>
 
 
